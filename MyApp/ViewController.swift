d426f08ff3a0ddcce6f961c36052bc273390f0ae
helmitaufiqa1 <drivehelmi09@gmail.com>
2023-01-23 16:00:16 +0700

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by mobile devmac22 on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
